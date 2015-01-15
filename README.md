fe173763bff2b0ae123ec95c33e38c80c2d8a716
Andrew Powell <powella@gilt.com>
2015-01-15 15:58:47 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # gulp-jasmine-phantomjs
-Gulp plugin to run client-side Jasmine tests with PhantomJS
+Gulp plugin to run client-side Jasmine 1.x tests with PhantomJS
+
+
+# [gulp](https://github.com/wearefractal/gulp)-mocha-phantomjs [![Build Status](https://travis-ci.org/gilt/gulp-jasmine-phantomjs.svg?branch=master)](https://travis-ci.org/gilt/gulp-jasmine-phantomjs)
+> A Gulp plugin to run client-side [Jasmine](http://jasmine.github.io/) tests with [PhantomJS](https://github.com/ariya/phantomjs)
+
+### Attribution
+This plugin was created with a significant amount of inspiration from [gulp-mocha-phantomjs](https://github.com/mrhooray/gulp-mocha-phantomjs).
+
+## Installation
+### node
+```shell
+$ npm install gulp-jasmine-phantomjs --save-dev
+```
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var fs = require('fs'),
+  path = require('path'),
+  url = require('url'),
+  spawn = require('child_process').spawn,
+  through = require('through2'),
+  gutil = require('gulp-util'),
+  pluginName = 'gulp-jasmine-phantomjs';
+
+function jasminePhantomJS (options) {
+  options = options || {};
+
+  var scriptPath = require.resolve('jasmine-phantomjs.js');
+
+  if (!scriptPath) {
+    throw new gutil.PluginError(pluginName, 'jasmine-phantomjs.js not found');
+  }
+
+  return through.obj(function (file, enc, cb) {
+    var args = [
+      scriptPath,
+      file.path,
+      JSON.stringify(options.phantomjs || {})
+    ];
+
+    spawnPhantomJS(args, options, function (err) {
+      if (err) {
+        this.emit('error', err);
+      }
+
+      this.push(file);
+
+      cb();
+    }.bind(this));
+  });
+}
+
+function spawnPhantomJS(args, options, cb) {
+
+  var phantomjsPath = path.join(path.dirname(require.resolve('phantomjs')), '../bin/phantomjs'),
+    errors = [],
+    phantomjs;
+
+  if (!phantomjsPath) {
+    return cb(new gutil.PluginError(pluginName, 'PhantomJS not found'));
+  }
+
+  phantomjs = spawn(phantomjsPath, args);
+
+  phantomjs.stdout.pipe(process.stdout);
+
+  phantomjs.stderr.on('data', function (data) {
+    errors.push(data);
+  });
+
+  phantomjs.on('error', function (err) {
+    cb(new gutil.PluginError(pluginName, err.message));
+  });
+
+  phantomjs.on('exit', function (code) {
+    if (errors.length) {
+      for(var i = 0; i < errors.length; i++){
+        console.log('[phantom] ' + errors[i]);
+      }
+    }
+
+    if (code === 0 || options.silent) {
+      cb();
+    } else {
+      cb(new gutil.PluginError(pluginName, 'Tests Failed: PhantomJS exited with code: ' + code));
+    }
+  });
+}
+
+module.exports = jasminePhantomJS
